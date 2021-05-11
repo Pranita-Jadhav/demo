78d9a6f329bc6a701b5958fabf97d341767de018
Pranita-Jadhav <82991818+Pranita-Jadhav@users.noreply.github.com>
2021-05-11 07:59:20 +0530

    Added extraa lines

--- src/main/java/com/java_programs/App.java
+++ src/main/java/com/java_programs/App.java
@@ -9,6 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        sysout.out.println("good morning");
+        System.out.println("good morning");
+        System.out.println("Hii");
     }
 }
